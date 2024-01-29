3ce9fd24a235b9867119f106420d0fc771a92ac9
Souradip <dgsouro77@gmail.com>
2024-01-30 02:27:31 +0530

    Updated logOut Button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 //import Home from './Pages/Home'
-//import Profile from './Pages/Profile/Profile'
-import Auth from './Pages/Authentication/Auth'
+import Profile from './Pages/Profile/Profile'
+//import Auth from './Pages/Authentication/Auth'
 function App() {
  
 
@@ -12,8 +12,8 @@ function App() {
       <div className="bg" style={{top:'-18%',right:'0'}}></div>
       <div className="bg" style={{top:'36%',left:'-8rem'}}></div>
     {/*  <Home/> */}
-   { /* <Profile/> */}
-   <Auth/>
+    <Profile/> 
+   {/*<Auth/>*/}
      </div>
 
     </>
